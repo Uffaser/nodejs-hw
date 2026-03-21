e3eecace671b85f728b6438f876d28a5e33a4d25
Uffaser <gerasimenkom405@gmail.com>
2026-03-21 14:05:19 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
-import { connectMongoDB } from './db/connectMangoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
